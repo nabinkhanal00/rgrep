8ccd99ae9c0c0ccbe525d57e06df144be96439cc
Nabin Khanal <nabinkhanal@skiff.com>
2023-12-04 22:54:29 +0545

    complete

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use rgrep::Config;
+use std::{env, process};
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = rgrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
